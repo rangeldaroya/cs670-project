015275b0bf33a73ececec7d04cf0f8f9d4fd878f
Your Name <rdaroya@umass.edu>
2022-11-30 11:03:26 -0500

    Remove unused libraries

--- src/lime_oxford102.py
+++ src/lime_oxford102.py
@@ -2,11 +2,9 @@ import matplotlib.pyplot as plt
 from PIL import Image
 import torch.nn as nn
 import numpy as np
-import os, json
 
 import torch
-from torchvision import models, transforms
-from torch.autograd import Variable
+from torchvision import transforms
 import torch.nn.functional as F
 import torchvision
 from torchvision.models import resnet50, ResNet50_Weights
