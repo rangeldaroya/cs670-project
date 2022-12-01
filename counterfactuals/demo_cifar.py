38caaba88cc93c49178a3d31497160f7b8ef889a
rdaroya_umass_edu <rdaroya_umass_edu@login2.unity.rc.umass.edu>
2022-12-01 22:31:13 +0000

    Change demo scripts for scve to use custom dataloader with image transformations

--- counterfactuals/demo_cifar.py
+++ counterfactuals/demo_cifar.py
@@ -11,6 +11,7 @@ import torchvision
 from torchvision import transforms
 
 from utils.visualize import visualize_counterfactuals
+from data.cifar import CIFAR10
 
 
 idx2label = ["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
@@ -19,11 +20,14 @@ idx2label = ["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "ho
 
 
 def main():
+    rot_vals_deg = np.loadtxt("/home/rdaroya_umass_edu/Documents/cs670-project/counterfactuals/scve_cifar_rot_vals_deg.txt")
+    trans_vals = np.loadtxt("/home/rdaroya_umass_edu/Documents/cs670-project/counterfactuals/scve_cifar_trans_vals.txt")
+    scales = np.loadtxt("/home/rdaroya_umass_edu/Documents/cs670-project/counterfactuals/scve_cifar_scales.txt")
     # args = parser.parse_args()
 
     # experiment_name = os.path.basename(args.config_path).split(".")[0]
     # dirpath = os.path.join(Path.output_root_dir(), experiment_name)
-    dirpath = "/home/rdaroya_umass_edu/Documents/cs670-project/counterfactuals/output/counterfactuals_ours_cifar_res50"
+    # dirpath = "/home/rdaroya_umass_edu/Documents/cs670-project/counterfactuals/output/counterfactuals_ours_cifar_res50"
 
     # dataset = get_test_dataset(get_vis_transform(), return_image_only=True)
 
@@ -32,14 +36,23 @@ def main():
         std=[0.229, 0.224, 0.225]
     )
     trans = transforms.Compose([transforms.Resize(224), transforms.CenterCrop(224), transforms.ToTensor(), normalize])
-    dataset = torchvision.datasets.CIFAR10(
-        root='./data', train=False, download=True, transform=trans)
+    dataset = CIFAR10(
+        root='./data', train=False, download=True, transform=trans,
+        rot_vals_deg=rot_vals_deg, trans_vals=trans_vals, scales=scales,
+    )
 
+    # counterfactuals = np.load(
+    #     os.path.join(dirpath, "counterfactuals.npy"), allow_pickle=True
+    # ).item()
+    cp_path = "/home/rdaroya_umass_edu/Documents/cs670-project/counterfactuals/cifar_counterfactuals.npy"
     counterfactuals = np.load(
-        os.path.join(dirpath, "counterfactuals.npy"), allow_pickle=True
+        cp_path, allow_pickle=True
     ).item()
 
-    for idx in np.random.choice(list(counterfactuals.keys()), 5):
+    cf_keys = list(counterfactuals.keys())
+    # for idx in np.random.choice(list(counterfactuals.keys()), 5):
+    for ctr, idx in enumerate(cf_keys):
+        print(f"Processing {ctr+1}/{len(cf_keys)}")
         cf = counterfactuals[idx]
 
         visualize_counterfactuals(
--- counterfactuals/demo_oxford.py
+++ counterfactuals/demo_oxford.py
@@ -11,6 +11,7 @@ import torchvision
 from torchvision import transforms
 
 from utils.visualize import visualize_counterfactuals
+from data.flowers102 import Flowers102
 
 
 idx2label = ['pink primrose', 'hard-leaved pocket orchid', 'canterbury bells', 'sweet pea', 'english marigold', 'tiger lily', 'moon orchid', 'bird of paradise', 'monkshood', 'globe thistle', 'snapdragon', "colt's foot", 'king protea', 'spear thistle', 'yellow iris', 'globe-flower', 'purple coneflower', 'peruvian lily', 'balloon flower', 'giant white arum lily', 'fire lily', 'pincushion flower', 'fritillary', 'red ginger', 'grape hyacinth', 'corn poppy', 'prince of wales feathers', 'stemless gentian', 'artichoke', 'sweet william', 'carnation', 'garden phlox', 'love in the mist', 'mexican aster', 'alpine sea holly', 'ruby-lipped cattleya', 'cape flower', 'great masterwort', 'siam tulip', 'lenten rose', 'barbeton daisy', 'daffodil', 'sword lily', 'poinsettia', 'bolero deep blue', 'wallflower', 'marigold', 'buttercup', 'oxeye daisy', 'common dandelion', 'petunia', 'wild pansy', 'primula', 'sunflower', 'pelargonium', 'bishop of llandaff', 'gaura', 'geranium', 'orange dahlia', 'pink-yellow dahlia', 'cautleya spicata', 'japanese anemone', 'black-eyed susan', 'silverbush', 'californian poppy', 'osteospermum', 'spring crocus', 'bearded iris', 'windflower', 'tree poppy', 'gazania', 'azalea', 'water lily', 'rose', 'thorn apple', 'morning glory', 'passion flower', 'lotus', 'toad lily', 'anthurium', 'frangipani', 'clematis', 'hibiscus', 'columbine', 'desert-rose', 'tree mallow', 'magnolia', 'cyclamen', 'watercress', 'canna lily', 'hippeastrum', 'bee balm', 'ball moss', 'foxglove', 'bougainvillea', 'camellia', 'mallow', 'mexican petunia', 'bromelia', 'blanket flower', 'trumpet creeper', 'blackberry lily']
@@ -19,6 +20,9 @@ idx2label = ['pink primrose', 'hard-leaved pocket orchid', 'canterbury bells', '
 
 
 def main():
+    rot_vals_deg = np.loadtxt("/home/rdaroya_umass_edu/Documents/cs670-project/counterfactuals/scve_oxford_rot_vals_deg.txt")
+    trans_vals = np.loadtxt("/home/rdaroya_umass_edu/Documents/cs670-project/counterfactuals/scve_oxford_trans_vals.txt")
+    scales = np.loadtxt("/home/rdaroya_umass_edu/Documents/cs670-project/counterfactuals/scve_oxford_scales.txt")
     # args = parser.parse_args()
 
     # experiment_name = os.path.basename(args.config_path).split(".")[0]
@@ -32,10 +36,13 @@ def main():
         std=[0.229, 0.224, 0.225]
     )
     trans = transforms.Compose([transforms.Resize(224), transforms.CenterCrop(224), transforms.ToTensor(), normalize])
-    dataset = torchvision.datasets.Flowers102(
-        root='./data', split='test', download=True, transform=trans)
+    dataset = Flowers102(
+        root='./data', split='test', download=True, transform=trans,
+        rot_vals_deg=rot_vals_deg, trans_vals=trans_vals, scales=scales,
+    )
 
-    cp_path = "/home/rdaroya_umass_edu/Documents/cs670-project/counterfactuals/oxford_counterfactuals_no_trans.npy"
+    # cp_path = "/home/rdaroya_umass_edu/Documents/cs670-project/counterfactuals/oxford_counterfactuals_no_trans.npy"
+    cp_path = "/home/rdaroya_umass_edu/Documents/cs670-project/counterfactuals/oxford_counterfactuals.npy"
     counterfactuals = np.load(
         cp_path, allow_pickle=True
     ).item()
